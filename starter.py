04418ffe2e234f365b64d08e1354494c1151fc72
Spencer Brazell <sa.brazell11@gmail.com>
2022-04-21 20:36:50 -0600

    problems working

--- starter.py
+++ starter.py
@@ -1,45 +1,53 @@
 # PROBLEM 1
 # Create a variable that holds the value of your first name.
-
+first_name = "Spencer"
 
 # PROBLEM 2
 # Create a variable that holds the value of your favorite number.
-
+favorite_number = 11
 
 # PROBLEM 3
 # Create a variable that holds a boolean value representing if your hair is brown.
-
+hair_is_brown = True
 
 # PROBLEM 4
 # Print your first name, by printing the variable created in problem 1.
-
+print(first_name)
 
 # PROBLEM 5
 #  Create a variable called `loves_code` and set it equal to true. 
 #  Check to see if `loves_code` is equal to true or false. 
 #  If it is true, print "I love to code!"
 #  If it is not, print "Coding has it's challenges."
-
+loves_code = True
+if loves_code == True:
+    print("I love to code!")
+else:
+    print("Coding has it's challenges.")
 
 # PROBLEM 6
 # Create an array called `colors` and set it equal to a list of at least five colors.
-
+colors = ['black', 'yellow', 'green', 'blue', 'red']
 
 # Problem 7
 # Using bracket syntax, print out the last item in your colors array.
-
+print(colors[4])
 
 # For problems 8-9, use the following line of code:
 numbers = [1,2,3,4,5,6,7,8,9,10]
 
 # Problem 8
 # Use a for-in loop to iterate over the `numbers` array and print each number.
-
+for num in numbers:
+    print(num)
 
 # Problem 9
 # Create an empty array called `even_numbers`.
 # Use a for-in loop to iterate over the `numbers` array, and if a number is even, add  it to the `even_numbers` array.
-
+even_numbers = []
+for num in numbers:
+    if num % 2 == 0:
+        even_numbers.append(num)
 
 # Problem 10
 # Do not edit the code below.
@@ -47,12 +55,27 @@ score = 74
 # Do not edit the code above.
 
 # Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console-log an 'A', between 80 and 89, console-log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console-log an 'F'.
-
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F") 
 
 # Problem 11
 # Create a variable called 'changeMyMind' and set it equal to true. 
 # Check to see if changeMyMind is set to true or false, if it is true, change the status to false, if it is false, change the status to true.
 
+
+change_my_mind = True
+if change_my_mind == True:
+  change_my_mind = False
+else:
+  change_my_mind = True
 
 # ADVANCED
 
