16960565597083439ea4c9e14adec3c5f94fada7
Spencer Brazell <sa.brazell11@gmail.com>
2022-04-21 21:41:51 -0600

    finished

---
+++ data_presenter.py
@@ -0,0 +1,32 @@
+
+# 2
+open_file = open("CupcakeInvoices.csv")
+
+#3
+for line in open_file:
+    print(line)
+
+#4
+for line in open_file:
+    values = line.strip(',')
+    print(values[2])
+    # for value in values:
+    #     if value 
+
+# 5
+for line in open_file:
+  values = line.split(',')
+  total = int(values[3]) * float(values[4])
+  print(total)
+
+#6
+total = 0
+
+for line in open_file:
+  values = line.split(',')
+  total = total + (int(values[3]) * float(values[4]))
+
+print(total)
+
+#7
+open_file.close()
